e64afd9f5727a0a41155d4e50720eb8e8838c000
Arif Hossan <arif.hossan.eu@gmail.com>
2023-11-08 23:22:07 +0600

    include react toastify for contact toast

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,9 +2,28 @@ import { Container } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import CustomButton from "../CustomButton/CustomButton";
+// import { toast } from "react-toastify";
+
 function Contact() {
-    const handleSubmit = () =>{
-        alert('Thanks')
+    // const handleSubmit = () =>{
+    //     alert('Thanks')
+    // }
+
+    const handleContactToast = () =>{
+      // toast('Thanks , This is a demo submission')
+      toast.success('Thanks, This is a demo submission!', {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+        });
     }
   return (
     <Container className="mt-5 mb-5">
@@ -30,13 +49,28 @@ function Contact() {
             placeholder="Enter your message"
           />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
           <Form.Check type="checkbox" label="Send me a copy" />
-        </Form.Group>
-        <Button onClick={handleSubmit} className='category-btn me-auto' type="submit">
+        </Form.Group> */}
+        {/* <Button onClick={handleContactToast} className='category-btn me-auto' type="submit">
           Submit
-        </Button>
+        </Button> */}
+
+        <CustomButton  text="Submit"
+            onClick={handleContactToast}></CustomButton>
       </Form>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
     </Container>
   );
 }
--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -1,16 +1,30 @@
-import React from 'react';
-import NavBar from '../components/Navbar/Navbar';
-import { Outlet } from 'react-router-dom';
-import Footer from '../components/Footer/Footer';
+import React from "react";
+import NavBar from "../components/Navbar/Navbar";
+import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer/Footer";
+// import { ToastContainer } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
 
 const Main = () => {
-    return (
-        <div>
-            <NavBar></NavBar>
-            <Outlet></Outlet>
-            <Footer></Footer>
-        </div>
-    );
+  return (
+    <div>
+      <NavBar></NavBar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+      {/* <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      /> */}
+    </div>
+  );
 };
 
 export default Main;
